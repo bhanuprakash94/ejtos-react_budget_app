6e664bcc0daebc4173d8b348d870e7d68039b127
bhanuprakash94 <venkatabhanuprakash.n@gmail.com>
2023-07-24 23:03:56 +0530

    Update ChangeCurrency.js

--- src/components/ChangeCurrency.js
+++ src/components/ChangeCurrency.js
@@ -1,36 +1,54 @@
-import React,{useContext, useState} from 'react';
+import React,{useContext,useState} from 'react';
 import  {AppContext} from '../context/AppContext';
 import './ChangeCurrency.css';
 
 const ChangeCurrency = () =>{
     const { dispatch,currency} = useContext(AppContext);
+    const [isOpen, setIsOpen] = useState(false);
+    let CurrencyName='';
     const setCurrency = (value) => {
         dispatch({
             type: 'CHG_CURRENCY',
             payload: value,
         });
+        CurrencyFind(value);
+        setIsOpen(!isOpen);
     };
      
-
     const currencies=[
         {'name': '$ Dollar','value':'$'},
         {'name': '£ Pound','value':'£'},
         {'name': '€ Euro','value':'€'},
         {'name': '₹ Ruppee','value':'₹'},
- 
- 
    ];
-    return (
-        <div className=''>
 
-            
-            <select value={currency} aria-label=".form-select-sm example" class="changeCurrency form-select form-select-sm alert alert-success" onChange={(event) => setCurrency(event.target.value)}>
-            {currencies.map((currencyData) => (
-                <option value={currencyData.value} >{currencyData.name}</option>
-            ))}
-            </select>
-            
-        </div>
+
+   const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+   };
+
+   CurrencyFind(currency);
+   function CurrencyFind(currency1){
+    const currencyData = currencies.find(({ name,value }) => value === currency1);
+
+    CurrencyName = 'Currency (' +currencyData.name+')';
+   }
+  
+    return (
+        <div onClick={toggleDropdown} className="custom-dropdown changeCurrency form-select form-select-sm alert alert-success">
+         <label  style={{
+             fontSize: '16px',
+             fontWeight: 600}}>{CurrencyName}</label>
+            {isOpen && (
+                <ul className='currency_ul'>
+                {currencies.map((option, index) => (
+                    <li key={option.value} onClick={() => setCurrency(option.value)}>
+                    {option.name}
+                    </li>
+                ))}
+                </ul>
+            )}
+    </div>
     );
 };
  
